88561a1b536c633c2180fa3100695b4dc7415801
LST <dev.st1027@gmail.com>
2023-06-15 16:14:41 +0900

    예외처리 실패

---
+++ src/main/java/com/codestates/response/ErrorResponse.java
@@ -0,0 +1,104 @@
+package com.codestates.response;
+
+import com.codestates.exception.BusinessLogicException;
+import lombok.Getter;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+
+import javax.validation.ConstraintViolation;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@Getter
+public class ErrorResponse {
+    private BusinessLogicError businessLogicError;
+    private List<FieldError> fieldErrors;
+    private List<ConstraintViolationError> violationErrors;
+
+    private ErrorResponse(final BusinessLogicError businessLogicError,
+                          final List<FieldError> fieldErrors,
+                          final List<ConstraintViolationError> violationErrors) {
+
+        this.businessLogicError = businessLogicError;
+        this.fieldErrors = fieldErrors;
+        this.violationErrors = violationErrors;
+    }
+
+    public static ErrorResponse of(BindingResult bindingResult) {
+        return new ErrorResponse(null, FieldError.of(bindingResult), null);
+    }
+
+    public static ErrorResponse of(Set<ConstraintViolation<?>> violations) {
+        return new ErrorResponse(null, null, ConstraintViolationError.of(violations));
+    }
+
+    public static ErrorResponse of(BusinessLogicException businessLogicException) {
+        return new ErrorResponse(BusinessLogicError.of(businessLogicException), null, null);
+    }
+
+    @Getter
+    public static class FieldError {
+        private String field;
+        private Object rejectedValue;
+        private String reason;
+
+        private FieldError(String field, Object rejectedValue, String reason) {
+            this.field = field;
+            this.rejectedValue = rejectedValue;
+            this.reason = reason;
+        }
+
+        public static List<FieldError> of(BindingResult bindingResult) {
+            final List<org.springframework.validation.FieldError> fieldErrors =
+                                                        bindingResult.getFieldErrors();
+            return fieldErrors.stream()
+                    .map(error -> new FieldError(
+                            error.getField(),
+                            error.getRejectedValue() == null ?
+                                            "" : error.getRejectedValue().toString(),
+                            error.getDefaultMessage()))
+                    .collect(Collectors.toList());
+        }
+    }
+
+    @Getter
+    public static class ConstraintViolationError {
+        private String propertyPath;
+        private Object rejectedValue;
+        private String reason;
+
+        private ConstraintViolationError(String propertyPath, Object rejectedValue,
+                                   String reason) {
+            this.propertyPath = propertyPath;
+            this.rejectedValue = rejectedValue;
+            this.reason = reason;
+        }
+
+        public static List<ConstraintViolationError> of(
+                Set<ConstraintViolation<?>> constraintViolations) {
+            return constraintViolations.stream()
+                    .map(constraintViolation -> new ConstraintViolationError(
+                            constraintViolation.getPropertyPath().toString(),
+                            constraintViolation.getInvalidValue().toString(),
+                            constraintViolation.getMessage()
+                    )).collect(Collectors.toList());
+        }
+    }
+
+    public static class BusinessLogicError {
+        private int status;
+        private String message;
+
+        private BusinessLogicError(int status, String message) {
+            this.status = status;
+            this.message = message;
+        }
+
+        public static BusinessLogicError of(BusinessLogicException e) {
+            return new BusinessLogicError(e.getExceptionCode().getStatus(), e.getMessage());
+        }
+    }
+
+
+}
